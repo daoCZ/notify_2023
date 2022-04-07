ceed7b02ef1854ec1fa50fd0a3dcd4fdc518acf3
matthewdelorenzo <matthewdelorenzo@tamu.edu>
2022-04-06 20:20:16 -0500

    addition of python function file and html version

--- testpy.py
+++ testpy.py
@@ -1,9 +1,16 @@
 from googleapiclient.discovery import build
 api_key = 'AIzaSyDEvFHFs6x4mq7qlV-9nED16_BlZm_N5BE'
 youtube = build('youtube', 'v3', developerKey = api_key)
-request = youtube.channels().list(
-    part = 'statistics',
-    forUsername = 'pewdiepie'
-    )
-response = request.execute()
-print(response.get('items'))
+
+def channel_search(user_query):
+    youtube = build('youtube', 'v3', developerKey = api_key)
+    request = youtube.search().list(
+            part="snippet",
+            maxResults=5,
+            q=user_query,
+            type="channel"
+        )
+    response = request.execute()
+    response_list = response.get('items')
+    return response_list
+
