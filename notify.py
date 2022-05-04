d2483ede8eca400bbadad0d1cf79212974a37683
AnzArkkala <anzarkkala@tamu.edu>
2022-05-03 21:19:18 -0500

    quick test

--- notify.py
+++ notify.py
@@ -239,51 +239,51 @@ def twitter_auth():
     return redirect('/')
 
 
-@app.route('/authorize')
-def authorize():
-    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-    flow = InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes='https://www.googleapis.com/auth/youtube.readonly')
+# @app.route('/authorize')
+# def authorize():
+#     # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+#     flow = InstalledAppFlow.from_client_secrets_file(
+#         CLIENT_SECRETS_FILE, scopes='https://www.googleapis.com/auth/youtube.readonly')
 
-    # The URI created here must exactly match one of the authorized redirect URIs
-    # for the OAuth 2.0 client, which you configured in the API Console. If this
-    # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
-    # error.
-    flow.redirect_uri = url_for('oauth2callback', _external=True)
+#     # The URI created here must exactly match one of the authorized redirect URIs
+#     # for the OAuth 2.0 client, which you configured in the API Console. If this
+#     # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+#     # error.
+#     flow.redirect_uri = url_for('oauth2callback', _external=True)
 
-    authorization_url, state = flow.authorization_url(
-        # Enable offline access so that you can refresh an access token without
-        # re-prompting the user for permission. Recommended for web server apps.
-        access_type='offline',
-        # Enable incremental authorization. Recommended as a best practice.
-        include_granted_scopes='true')
+#     authorization_url, state = flow.authorization_url(
+#         # Enable offline access so that you can refresh an access token without
+#         # re-prompting the user for permission. Recommended for web server apps.
+#         access_type='offline',
+#         # Enable incremental authorization. Recommended as a best practice.
+#         include_granted_scopes='true')
 
-    # Store the state so the callback can verify the auth server response.
-    session['state'] = state
+#     # Store the state so the callback can verify the auth server response.
+#     session['state'] = state
 
-    return redirect(authorization_url)
-@app.route('/oauth2callback')
-def oauth2callback():
-    # Specify the state when creating the flow in the callback so that it can
-    # verified in the authorization server response.
-    state = session['state']
+#     return redirect(authorization_url)
+# @app.route('/oauth2callback')
+# def oauth2callback():
+#     # Specify the state when creating the flow in the callback so that it can
+#     # verified in the authorization server response.
+#     state = session['state']
 
-    flow = InstalledAppFlow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes='https://www.googleapis.com/auth/youtube.readonly', state=state)
-    flow.redirect_uri = url_for('oauth2callback', _external=True)
+#     flow = InstalledAppFlow.from_client_secrets_file(
+#         CLIENT_SECRETS_FILE, scopes='https://www.googleapis.com/auth/youtube.readonly', state=state)
+#     flow.redirect_uri = url_for('oauth2callback', _external=True)
 
-    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-    authorization_response = request.url
-    flow.fetch_token(authorization_response=authorization_response)
+#     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+#     authorization_response = request.url
+#     flow.fetch_token(authorization_response=authorization_response)
 
-    # Store credentials in the session.
-    # ACTION ITEM: In a production app, you likely want to save these
-    #              credentials in a persistent database instead.
-    credentials = flow.credentials
-    session['credentials'] = dict(credentials)
+#     # Store credentials in the session.
+#     # ACTION ITEM: In a production app, you likely want to save these
+#     #              credentials in a persistent database instead.
+#     credentials = flow.credentials
+#     session['credentials'] = dict(credentials)
 
-    return redirect(url_for('google/'))
-@app.route('/google/')
+#     return redirect(url_for('google/'))
+@app.route('/google')
 def google():
     global toHTML
     global name2
